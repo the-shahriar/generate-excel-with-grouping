bcf03ffd0dad891d9a3d7ab27ce7ca34f1a400a0
Shahriar Islam <56726899+the-shahriar@users.noreply.github.com>
2025-01-02 00:30:24 +0600

    Update headers in main.py

--- main.py
+++ main.py
@@ -7,17 +7,7 @@ app = FastAPI()
 @app.get("/prepare_excel_data")
 async def prepare_excel_data(response: Response):
     headers = [
-        'B&H BG 20HL', 'B&H BG PUG 12HL', 'B&H BG LEP 20HL', 'B&H BG LEP 12HL', 'B&H BG 12HL', 
-        'B&H BG Total', 'B&H CRUSH 20HL', 'B&H Breeze 20HL LEPP', 'B&H Fusion 20HL LEPP', 
-        'B&H SW LEP 20HL', 'B&H SW 20HL', 'B&H SW Total', 'B&H SF LEP 12HL', 'B&H SF LEP 20HL', 
-        'B&H SF 20HL', 'B&H SF 12HL', 'B&H SF Total', 'Alchemy 7MG', 'Alchemy MIX1', 'Alchemy Total', 
-        'B&H PT 20HL', 'B&H Family', 'JP SW 20HL', 'JPGL 20HL', 'JPGL 12HL', 'JPGL Total', 'JP SP 20HL', 
-        'JP Family', 'CAP 20HL', 'Capstan Family', 'SRF 20HL', 'SRF 10HL', 'SRFT Total', 'Star Switch 20HL', 
-        'SRFT Family', 'PL 20HL', 'Pilot Family', 'HWD 20HL', 'HWG 20HL', 'Hollywood Total', 'Hollywood Family', 
-        'Derby Style 10HL', 'Derby Style 20HL', 'Derby ST 10HL LEP', 'Derby ST 20HL LEP', 'Derby Style Total', 
-        'DB Deluxe 10HL', 'DB Deluxe 20HL', 'DB Deluxe Total', 'DB Select 10HL', 'DB Select 20HL', 'DB Select Total', 
-        'Derby 10HL', 'Derby 20HL', 'Derby 20HL LEP', 'Derby 10HL LEP', 'Derby Total', 'Derby Family', 'RN 10HL', 
-        'RN 20HL', 'Royals Next Total', 'RG 20HL', 'RG 10HL', 'Royals Gold Total', 'RLS 10HL', 'Royals LC 10HL', 
+        'RN 10HL', 'RN 20HL', 'Royals Next Total', 'RG 20HL', 'RG 10HL', 'Royals Gold Total', 'RLS 10HL', 'Royals LC 10HL', 
         'Royals LC 20HL', 'Royals LC Total', 'Royals Family', 'LS OG 20HL', 'LS RED 20HL LEPP', 'LS FT 20HL LEPP', 
         'LS BC 20HL LEPP', 'LS CC 20HL', 'Lucky Family'
     ]
